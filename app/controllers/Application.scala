abbe0360b5e19aa77c311a63ffe124384f6efd2d
Fred <frederic.fyfer@gmail.com>
2017-05-11 19:38:16 +0200

    Add POST /user and POST /food endpoints

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,47 @@
 package controllers
 
-import play.api._
+import models.{Food, User}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
+import play.api.data.format.Formats._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  val userForm: Form[User] = Form {
+    mapping(
+      "email" -> email,
+      "password" -> text,
+      "weight" -> of(doubleFormat),
+      "height" -> of(doubleFormat),
+      "gender" -> text
+    )(User.apply)(User.unapply)
+  }
+
+  def addUser = Action { implicit request =>
+    val user = userForm.bindFromRequest.get
+    // TODO: Save user to database.
+    Redirect(routes.Application.index())
+  }
+
+  val foodForm: Form[Food] = Form {
+    mapping(
+      "name" -> text,
+      "calories" -> of(doubleFormat),
+      "proteins" -> of(doubleFormat),
+      "carbohydrates" -> of(doubleFormat),
+      "lipids" -> of(doubleFormat)
+    )(Food.apply)(Food.unapply)
+  }
+
+  def addFood = Action { implicit request =>
+    val food = foodForm.bindFromRequest.get
+    // TODO: Save food to database.
+    Redirect(routes.Application.index())
   }
 
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json.Json
 
-case class User(email: String, password: String, weight: Double, height: Double, gender: Char)
+case class User(email: String, password: String, weight: Double, height: Double, gender: String)
 
 object User {
   implicit val userFormat = Json.format[User]
